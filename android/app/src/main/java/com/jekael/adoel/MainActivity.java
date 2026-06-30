d64cadb438259553a8ff975f4b840f1fb1ca2b7e
Jekael <wahyumaulana925@gmail.com>
2026-06-30 11:00:09 +0000

    Setup Vite + Tailwind + Zustand + Capacitor + CI

---
+++ android/app/src/main/java/com/jekael/adoel/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jekael.adoel;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jekael.adoel',
+  appName: 'Adoel.',
+  webDir: 'dist'
+};
+
+export default config;
